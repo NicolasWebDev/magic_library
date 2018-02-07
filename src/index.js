0d44b17674bfe971e940784b92eee333ded77f3d
Sathors <sathors@free.fr>
2018-02-07 14:01:40 -0500

    Puppeteer is working.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('coucou')
+import 'babel-polyfill'
+import puppeteer from 'puppeteer'
+
+const launchArgs = {
+  headless: false,
+  args: ['--no-sandbox', '--disable-notifications'],
+  slowMo: 50
+};
+
+(async () => {
+  const browser = await puppeteer.launch(launchArgs)
+  const page = await browser.newPage()
+  await page.goto('https://amazon.com')
+  await page.screenshot({path: 'example.png'})
+
+  await browser.close()
+})()

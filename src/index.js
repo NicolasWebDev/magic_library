14038f2d7a0fbbb91c2d5775becf48dc2f522fec
Sathors <sathors@free.fr>
2018-05-19 13:12:43 -0500

    Catch the unresolved promises.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import 'babel-polyfill'
 import AmazonBook from './AmazonBook'
 
-(async () => {
+const main = async () => {
   const bookUrl = process.argv[2]
   const book = await AmazonBook.buildFromUrl(bookUrl)
   console.log(book.toString())
-})()
+}
+
+main()
+  .then(() => {}, error => { console.log(error) })
+  .then(() => process.exit())
